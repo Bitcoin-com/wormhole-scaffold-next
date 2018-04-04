0fb8c3adb9c2138fe1307fc8e1d3c0b1b69b1e13
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-04 15:01:37 -0700

    BIP44 basic wallet.

--- pages/index.js
+++ pages/index.js
@@ -11,151 +11,100 @@ let BITBOX = new BITBOXCli({
   password: ''
 });
 
-BITBOX.Mnemonic.generateMnemonic(256);
+let langs = [
+  'english',
+  'chinese_simplified',
+  'chinese_traditional',
+  'korean',
+  'japanese',
+  'french',
+  'italian',
+  'spanish'
+]
 
+let lang = langs[Math.floor(Math.random()*langs.length)];
+
+// create 256 bit BIP39 mnemonic
+let mnemonic = BITBOX.Mnemonic.generateMnemonic(256, BITBOX.Mnemonic.mnemonicWordLists()[lang])
+
+// mnemonic to BIP32 root seed encoded as hex
+let rootSeedHex = BITBOX.Mnemonic.mnemonicToSeedHex(mnemonic)
+
+// root seed to BIP32 master HD Node
+let masterHDNode = BITBOX.HDNode.fromSeedHex(rootSeedHex)
+
+// derive BIP 44 external receive address
+let childNode = masterHDNode.derivePath("m/44'/145'/0'/0/0")
+
+// instance of transaction builder
+let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
+
+// keypair of BIP44 receive address
+let keyPair = childNode.keyPair;
+
+// txid of utxo
+let txid = '5699610b1db28d77b1021ed457d5d9010900923143757bc8698083fa796b3307';
+
+// add input txid, vin 1 and keypair
+transactionBuilder.addInput(txid, 1, keyPair);
+
+// calculate fee @ 1 sat/B
+let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
+
+// subtract fee from original amount
+let originalAmount = 3678031;
+
+let sendAmount = originalAmount - byteCount;
+
+// add receive address and send amount
+transactionBuilder.addOutput('qpq57nsrhje3725fzxfjdqzdngep3cfk2sfmy8yexj', sendAmount);
+
+// sign tx
+transactionBuilder.sign(0, originalAmount);
+
+// build it and raw hex
+let tx = transactionBuilder.build();
+let hex = tx.toHex();
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      version: '',
-      protocolversion: '',
-      walletversion: '',
-      balance: '',
-      blocks: '',
-      timeoffset: '',
-      connections: '',
-      proxy: '',
-      difficulty: '',
-      testnet: '',
-      keypoololdest: '',
-      keypoolsize: '',
-      paytxfee: '',
-      relayfee: '',
-      errors: ''
+      mnemonic: mnemonic,
+      lang: lang,
+      hex: hex,
     }
   }
 
-  componentDidMount() {
-    BITBOX.Control.getInfo()
-    .then((result) => {
-      this.setState({
-        version: result.version,
-        protocolversion: result.protocolversion,
-        walletversion: result.walletversion,
-        balance: result.balance,
-        blocks: result.blocks,
-        timeoffset: result.timeoffset,
-        connections: result.connections,
-        proxy: result.proxy,
-        difficulty: result.difficulty,
-        testnet: result.testnet,
-        keypoololdest: result.keypoololdest,
-        keypoolsize: result.keypoolsize,
-        paytxfee: result.paytxfee,
-        relayfee:result.relayfee,
-        errors: result.errors
-      });
-    }, (err) => { console.log(err);
-    });
-  }
-
   render() {
+    let addresses = [];
+    for(let i = 0; i < 10; i++) {
+      let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
+      addresses.push(<li>m/44&rsquo;/145&rsquo;/0&rsquo;/0/{i}: {BITBOX.HDNode.toCashAddress(childNode)}</li>);
+    }
+
     return (
       <div className="App">
-	  	<Head />
-		<Nav />
         <header className="App-header">
+          <img src='static/logo.png' className="App-logo" alt="logo" />
           <h1 className="App-title">Hello BITBOX</h1>
         </header>
         <div className='App-content'>
-          <h2><code>getinfo</code></h2>
+          <h2>BIP44 $BCH Wallet</h2>
+          <h3>256 bit {lang} BIP39 Mnemonic:</h3> <p>{this.state.mnemonic}</p>
+          <h3>BIP44 Account</h3>
+          <p>
+            <code>
+            "m/44'/145'/0'"
+            </code>
+          </p>
+          <h3>BIP44 external change addresses</h3>
           <ul>
-            <li>
-                version: {this.state.version},
-            </li>
-            <li>
-                protocolversion: {this.state.protocolversion},
-            </li>
-            <li>
-                walletversion: {this.state.walletversion},
-            </li>
-            <li>
-                balance: {this.state.balance},
-            </li>
-            <li>
-                blocks: {this.state.blocks},
-            </li>
-            <li>
-                timeoffset: {this.state.timeoffset},
-            </li>
-            <li>
-                connections: {this.state.connections},
-            </li>
-            <li>
-                proxy: {this.state.proxy},
-            </li>
-            <li>
-                difficulty: {this.state.difficulty},
-            </li>
-            <li>
-                testnet: {this.state.testnet},
-            </li>
-            <li>
-                keypoololdest: {this.state.keypoololdest},
-            </li>
-            <li>
-                keypoolsize: {this.state.keypoolsize},
-            </li>
-            <li>
-                paytxfee: {this.state.paytxfee},
-            </li>
-            <li>
-                relayfee:{this.state.relayfee},
-            </li>
-            <li>
-                errors: {this.state.errors}
-            </li>
-            </ul>
+            {addresses}
+          </ul>
+          <h3>Transaction raw hex</h3>
+          <p>{this.state.hex}</p>
         </div>
-		<style jsx>{`
-			.App {
-  				text-align: center;
-			}
-
-			.App-logo {
-  				height: 80px;
-			}
-
-			.App-header {
-  				background-color: #2d3e50;
-  				height: 150px;
-  				padding: 20px;
-  				color: white;
-			}
-
-			.App-title {
-  				font-size: 1.5em;
-  				font-weight: 400;
-			}
-
-			.App-intro {
-  				font-size: large;
-			}
-
-			.App-content {
-  				padding-left: 20px;
-			}
-
-			.App ul, .App h2 {
-  				text-align: left;
-			}
-
-			@keyframes App-logo-spin {
-  				from { transform: rotate(0deg); }
-  				to { transform: rotate(360deg); }
-			}
-		`}</style>
       </div>
     );
   }
