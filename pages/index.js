90eae974be1402ba0d624b60d22dbfe9f9c3eadc
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-05 09:12:44 -0700

    v and componentDidMount

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import Head from '../components/head';
 import Nav from '../components/nav';
-
 let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
 let BITBOX = new BITBOXCli({
   protocol: 'http',
@@ -11,76 +10,88 @@ let BITBOX = new BITBOXCli({
   password: ''
 });
 
-let langs = [
-  'english',
-  'chinese_simplified',
-  'chinese_traditional',
-  'korean',
-  'japanese',
-  'french',
-  'italian',
-  'spanish'
-]
-
-let lang = langs[Math.floor(Math.random()*langs.length)];
-
-// create 256 bit BIP39 mnemonic
-let mnemonic = BITBOX.Mnemonic.generateMnemonic(256, BITBOX.Mnemonic.mnemonicWordLists()[lang])
-
-// mnemonic to BIP32 root seed encoded as hex
-let rootSeedHex = BITBOX.Mnemonic.mnemonicToSeedHex(mnemonic)
-
-// root seed to BIP32 master HD Node
-let masterHDNode = BITBOX.HDNode.fromSeedHex(rootSeedHex)
-
-// derive BIP 44 external receive address
-let childNode = masterHDNode.derivePath("m/44'/145'/0'/0/0")
-
-// instance of transaction builder
-let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
-
-// keypair of BIP44 receive address
-let keyPair = childNode.keyPair;
-
-// txid of utxo
-let txid = '5699610b1db28d77b1021ed457d5d9010900923143757bc8698083fa796b3307';
-
-// add input txid, vin 1 and keypair
-transactionBuilder.addInput(txid, 1, keyPair);
-
-// calculate fee @ 1 sat/B
-let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
-
-// subtract fee from original amount
-let originalAmount = 3678031;
-
-let sendAmount = originalAmount - byteCount;
-
-// add receive address and send amount
-transactionBuilder.addOutput('bitcoincash:qpuax2tarq33f86wccwlx8ge7tad2wgvqgjqlwshpw', sendAmount);
-
-// sign tx
-transactionBuilder.sign(0, originalAmount);
-
-// build it and raw hex
-let tx = transactionBuilder.build();
-let hex = tx.toHex();
-
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mnemonic: '',
+      lang: '',
+      hex: '',
+      masterHDNode: ''
+    }
+  }
+
+  componentDidMount() {
+    let langs = [
+      'english',
+      'chinese_simplified',
+      'chinese_traditional',
+      'korean',
+      'japanese',
+      'french',
+      'italian',
+      'spanish'
+    ]
+
+    let lang = langs[Math.floor(Math.random()*langs.length)];
+
+    // create 256 bit BIP39 mnemonic
+    let mnemonic = BITBOX.Mnemonic.generateMnemonic(256, BITBOX.Mnemonic.mnemonicWordLists()[lang])
+
+    // mnemonic to BIP32 root seed encoded as hex
+    let rootSeedHex = BITBOX.Mnemonic.mnemonicToSeedHex(mnemonic)
+
+    // root seed to BIP32 master HD Node
+    let masterHDNode = BITBOX.HDNode.fromSeedHex(rootSeedHex)
+
+    // derive BIP 44 external receive address
+    let childNode = masterHDNode.derivePath("m/44'/145'/0'/0/0")
+
+    // instance of transaction builder
+    let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
+
+    // keypair of BIP44 receive address
+    let keyPair = childNode.keyPair;
+
+    // txid of utxo
+    let txid = '5699610b1db28d77b1021ed457d5d9010900923143757bc8698083fa796b3307';
+
+    // add input txid, vin 1 and keypair
+    transactionBuilder.addInput(txid, 1, keyPair);
+
+    // calculate fee @ 1 sat/B
+    let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
+
+    // subtract fee from original amount
+    let originalAmount = 3678031;
+
+    let sendAmount = originalAmount - byteCount;
+
+    // add receive address and send amount
+    transactionBuilder.addOutput('bitcoincash:qpuax2tarq33f86wccwlx8ge7tad2wgvqgjqlwshpw', sendAmount);
+
+    // sign tx
+    transactionBuilder.sign(0, originalAmount);
+
+    // build it and raw hex
+    let tx = transactionBuilder.build();
+    let hex = tx.toHex();
+
+    this.setState({
       mnemonic: mnemonic,
       lang: lang,
       hex: hex,
-    }
+      masterHDNode: masterHDNode
+    });
   }
 
   render() {
     let addresses = [];
     for(let i = 0; i < 10; i++) {
-      let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
-      addresses.push(<li>m/44&rsquo;/145&rsquo;/0&rsquo;/0/{i}: {BITBOX.HDNode.toCashAddress(childNode)}</li>);
+      if(this.state.masterHDNode) {
+        let childNode = this.state.masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
+        addresses.push(<li key={i}>m/44&rsquo;/145&rsquo;/0&rsquo;/0/{i}: {BITBOX.HDNode.toCashAddress(childNode)}</li>);
+      }
     }
 
     return (
@@ -91,7 +102,7 @@ class Index extends React.Component {
         </header>
         <div className='App-content'>
           <h2>BIP44 $BCH Wallet</h2>
-          <h3>256 bit {lang} BIP39 Mnemonic:</h3> <p>{this.state.mnemonic}</p>
+          <h3>256 bit {this.state.lang} BIP39 Mnemonic:</h3> <p>{this.state.mnemonic}</p>
           <h3>BIP44 Account</h3>
           <p>
             <code>
